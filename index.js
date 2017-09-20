71f831b1362c9197ed7e657ff7edb5cfc4b66cf3
Fabiano Eger <fabinhoeger@hotmail.com>
2017-09-19 21:37:35 -0300

    bot init

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const express = require('express');
+const Telegraf = require('telegraf');
+const express  = require('express');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
+
+bot.command('start', ({from, reply}) => {
+    console.log('start', from);
+    return reply('Welcome!');
+});
+
+bot.startPolling();
 
 app.listen(app.get('port'), () => {
     console.log('Running on port', app.get('port'));
