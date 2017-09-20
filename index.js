7adba348f6be03fb5cfd96ff9bd8729d7c5bca1f
Fabiano Eger <fabinhoeger@hotmail.com>
2017-09-20 11:28:23 -0300

    added cep to bot

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Telegraf = require('telegraf');
 const express  = require('express');
 const getCep   = require('./services/cep.js');
+const trim     = require('./utils/trim.js');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
+
 
 app.set('port', process.env.PORT || 5000);
 
@@ -13,15 +15,28 @@ app.get('/', (req, res) => {
 
 bot.command('start', ({from, reply}) => {
     console.log('start', from);
-    return reply('Welcome!');
+    return reply('Bem vindo!');
 });
 
 bot.command('cep', ({from, reply, message}) => {
-    return reply('CEP test');
+    if (message.text.length <= 4) return reply('Você deve passar o número do seu CEP'); 
+    let cep = message.text.substring(message.entities[0].length, message.text.length);
+    cep = trim(cep);
+    const line_break = '\n';
+    getCep(cep).then((result) => {
+        let reply_msg = `Cidade: ${result.cidade} - ${result.estado} ${line_break}`;
+        reply_msg =  `${reply_msg}Área da Cidade: ${result.cidade_info.area_km2} KM² ${line_break}`;
+        reply_msg =  `${reply_msg}Área do Estado: ${result.estado_info.area_km2} KM²`;
+        return reply(reply_msg);
+    }).catch(() => reply('CEP inválido :(')); 
 });
 
 bot.command('rastrear', ({from, reply}) => {
-    return reply('Rastreae test');
+    return reply('Desculpe, eu ainda não fui preparado para responder isso! :(');
+});
+
+bot.command('', ({reply}) => {
+    reply('Use /cep espaco número do seu CEP ou /rastrear código de rastreio do seu objeto');
 });
 
 bot.startPolling();
